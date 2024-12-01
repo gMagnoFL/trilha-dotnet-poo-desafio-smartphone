b5a0398312f806958ec082c9135f0f38b9669e7f
gMagnoFL <gustavomagno15@gmail.com>
2024-12-01 00:44:32 -0300

    Resolução

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,20 @@
-namespace DesafioPOO.Models
+﻿namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
-    {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
-    }
+   
+
+ // TODO: Herdar da classe "Smartphone"
+    
+public class Iphone
+ : Smartphone   {
+
+
+public Iphone(string Numero, string Modelo, string IMEI, int Memoria) : base(numero, modelo, imei, memoria)  { }
+// TODO: Sobrescrever o método "InstalarAplicativo"
+				public override void InstalarAplicativo(string nomeApp){
+			Console.WriteLine($"Instalando '{nomeApp}'");
+
+			
+			Console.WriteLine($"'{nomeApp}' instalado com sucesso");
+}
+}
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,20 @@
-namespace DesafioPOO.Models
+﻿namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
-    {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
-    }
+
+
+// TODO: Herdar da classe "Smartphone"
+		
+
+public class Nokia : Smartphone   {
+
+
+public Nokia(string Numero, string Modelo, string IMEI, int Memoria) : base(numero, modelo, imei, memoria)  { }
+// TODO: Sobrescrever o método "InstalarAplicativo"
+				public override void InstalarAplicativo(string nomeApp){
+			Console.WriteLine($"Instalando '{nomeApp}'");
+
+			
+			Console.WriteLine($"'{nomeApp}' instalado com sucesso");
+}
+}
 }
